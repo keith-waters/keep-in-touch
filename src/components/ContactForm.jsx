a13b17600dd79ab3c09946c1ac56e27e091f21f5
keith-waters <readingwaters@gmail.com>
2022-04-18 00:29:25 -0400

    PoC for creating multiple contacts at one time

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import styled from '@emotion/styled'
 import axios from 'axios'
 import Card from '@mui/material/Card'
 import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
-import Link from 'next/link'
 
 const StyledCard = styled(Card)`
 	padding: 5px;
@@ -12,60 +10,36 @@ const StyledCard = styled(Card)`
 `
 
 const ContactForm = (props) => {
-	const initialValues = {
-		firstName: '',
-		lastName: '',
-		emailAddress: '',
-		phoneNumber: ''
-	}
-	const [values, setValues] = useState(initialValues)
-
-	const handleSubmit = async () => {
-		const res = await axios.post('/api/contacts', values)
-	}
-	
 	return (
 		<StyledCard variant='outlined'>
 			<TextField
 				label='First Name'
 				size='small'
 				margin='normal'
-				value={values.firstName}
-				onChange={(ev) => {setValues({...values, firstName: ev.target.value})}}
+				value={props.firstName}
+				onChange={(ev) => {props.handleChange(props.index, {firstName: ev.target.value})}}
 			/>
 			<TextField
 				label='Last Name'
 				size='small'
 				margin='normal'
-				value={values.lastName}
-				onChange={(ev) => {setValues({...values, lastName: ev.target.value})}}
+				value={props.lastName}
+				onChange={(ev) => {props.handleChange(props.index, {lastName: ev.target.value})}}
 			/>
 			<TextField
 				label='Email'
 				size='small'
 				margin='normal'
-				value={values.emailAddress}
-				onChange={(ev) => {setValues({...values, emailAddress: ev.target.value})}}
+				value={props.emailAddress}
+				onChange={(ev) => {props.handleChange(props.index, {emailAddress: ev.target.value})}}
 			/>
 			<TextField
 				label='Phone'
 				size='small'
 				margin='normal'
-				value={values.phoneNumber}
-				onChange={(ev) => {setValues({...values, phoneNumber: ev.target.value})}}
+				value={props.phoneNumber}
+				onChange={(ev) => {props.handleChange(props.index, {phoneNumber: ev.target.value})}}
 			/>
-			<br/>
-			<Link href='/'>
-				<Button>
-					Cancel
-				</Button>
-			</Link>
-			<Button
-				type='submit'
-				onClick={handleSubmit}
-			>
-				Submit
-			</Button>
 		</StyledCard>
 	)
 
--- src/pages/create.js
+++ src/pages/create.js
@@ -1,9 +1,60 @@
+import { useState } from 'react'
+import axios from 'axios'
+import Button from '@mui/material/Button'
+import Link from 'next/link'
 import ContactForm from '../components/ContactForm'
 
 export default function Create() {
+	const initialValues = {
+		firstName: '',
+		lastName: '',
+		emailAddress: '',
+		phoneNumber: ''
+	}
+	const [formData, setFormData] = useState([initialValues])
+
+	const handleAdd = () => {
+		if (formData.length < 5) setFormData([...formData, initialValues])
+	}
+
+	const handleChange = (index, data) => {
+		const contacts = [...formData]
+		contacts[index] = {...contacts[index], ...data}
+		setFormData(contacts)
+	}
+
+	const handleSubmit = async () => {
+		formData.forEach(async (data) => {
+			const res = await axios.post('/api/contacts', data)
+		})
+	}
+
   return (
 		<>
-			<ContactForm />
+			{formData.map((data, index) => (
+				<ContactForm 
+					key={index} 
+					index={index} 
+					handleChange={handleChange} 
+					{ ...data }
+				/>
+			))}
+			<Button
+				onClick={handleAdd}
+			>
+			  Add another contact	
+			</Button>
+			<Link href='/'>
+				<Button>
+					Cancel
+				</Button>
+			</Link>
+			<Button
+				type='submit'
+				onClick={handleSubmit}
+			>
+				Submit
+			</Button>
 		</>
   )
 }
